c2fd507d13cdb201690a5a2f8b871e902b71a1c8
Loïc Delmaire <loic.delmaire@gmail.com>
2024-01-21 19:37:33 +0100

    Initial commit

---
+++ lib/sniprails/cli.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "cli/version"
+
+module Sniprails
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sniprails/cli/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Sniprails
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
